2688b59de3611cc292d3e180b6a808a3d3d19461
KALIANE NICOLLY FERREIRA DA SILVA <20231170150044@ifrn.local>
2026-09-01 14:29:40 -0300

    primeira fase pronta

--- dialogue4v2026/Assets/Scripts/Checkpoint.cs
+++ dialogue4v2026/Assets/Scripts/Checkpoint.cs
@@ -6,6 +6,11 @@ public class Checkpoint : MonoBehaviour
 
     private bool activated;
 
+    private void Start()
+    {
+        respawnPoint = this.transform;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player"))
